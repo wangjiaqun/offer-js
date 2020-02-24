eb48d87abb46881c2aff98470e7cec672d225bab
wangjiaqun <250402414@qq.com>
2020-02-24 21:15:24 +0800

    23题 未完成

--- src/23_entry-node-of-loop.js
+++ src/23_entry-node-of-loop.js
@@ -1,8 +1,14 @@
-function ListNode(x){
+/**
+ * @description 链表中环的入口结点
+ * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ * @author wangjiaqun
+ * @param
+ * @return
+ */
+function ListNode(x) {
     this.val = x;
     this.next = null;
 }
-function EntryNodeOfLoop(pHead)
-{
+function entryNodeOfLoop(pHead) {
     // write code here
 }
